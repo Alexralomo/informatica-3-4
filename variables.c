0cb1c2a4ce1d3bced21d7f1a6445dfb705386a01
Alexralomo <manuelralomo@gmail.com>
2026-04-21 14:55:16 +0000

    varibles

---
+++ variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+ int number;  // esto no es exacto lo del final por eso esta mal con esta corexion se resuelve
+
+   int test_value; //le falto diagonal y por eso esta mal el programa
+   int example1; //no de ve empesar con un numero puede terminar pero no iniciar
+   float int_Float; // se le tiene que poner el nomvre a la variable
+   printf("Variables created successfully.\n");
+}
