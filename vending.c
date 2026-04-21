5c5643b663e1b2a2de5b7240afac26e4dc955acd
Alexralomo <manuelralomo@gmail.com>
2026-04-21 15:48:57 +0000

    Variables

---
+++ vending.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("Vending Machine Stock\n"); // Es el titulo
+    printf("-----------------------\n");
+    //Enchiladas variables
+ float price1= 83.3;
+ int quantity1= 20;
+ char select1='a';
+
+}
