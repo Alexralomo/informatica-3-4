182530b19f8c114c4d53c26b7babee4fb0036c0e
Alexralomo <manuelralomo@gmail.com>
2026-04-22 15:40:03 +0000

    vending

--- vending.c
+++ vending.c
@@ -3,7 +3,7 @@
 int main(void){
 
     printf("Vending Machine Stock\n"); // Es el titulo
-    printf("-----------------------\n");
+    printf("---------------------------------------------------------\n");
 
    printf("Item\t     price      Quantity    Code\n");
 
@@ -11,16 +11,31 @@ int main(void){
  float price1= 83;
  int quantity1= 20;
  char select1='a';
-  printf("Tacos  \t     %f     %d        %c   \n", price1, quantity1, select1);
+  printf("Tacos many\5t\t%f\t%d\t%c\t\n", price1, quantity1, select1);
 
  //lo sigiente sera unos Coca cola 🌮🌮🌮🌮🌮🌮🌮🌮
 float price2= 27.99;
-int quantitly2= ;
+int quantitly2= 10;
 char select2='b';
-printf("Coca cola    %f     %d        %c   \n", price2, quantitly2, select2);
+printf("Coca cola\t%f\t%d\t%c\t\n", price2, quantitly2, select2);
+printf("--------------------------------------------------------------\n");
+                                      printf("Vending Machine Stock\n");
 
-//Tortas chumas
-float price3
+printf("--------------------------------------------------------------\n");
+                                      printf("Item: Tacos\n");
+    printf("Price $%  2f\n",price1);
+     printf("Quantity %d\n",quantity1);
+     printf("Code %c\n",select1);
+    printf("\n");
+       printf("Item: coca cola\n");
+        printf("Price $%  2f\n",price2);
+        printf("Quantity %d\n",quantitly2);
+        printf("Code %c\n",select2);
+
+
+
+
+
 
 
 }
