f27b01c25b73c7b0bb1b12fd53147b69d7f35207
Alexralomo <manuelralomo@gmail.com>
2026-04-22 01:14:42 +0000

    vending

--- vending.c
+++ vending.c
@@ -4,9 +4,27 @@ int main(void){
 
     printf("Vending Machine Stock\n"); // Es el titulo
     printf("-----------------------\n");
-    //Enchiladas variables
- float price1= 83.3;
+
+   printf("Item\t     price      Quantity    Code\n");
+
+    //Tacossssss variables 🌯🌯🌯🌯🌯🌯🌯🌯🌯
+ float price1= 83;
  int quantity1= 20;
  char select1='a';
+  printf("Tacos  \t     %f     %d        %c   \n", price1, quantity1, select1);
+
+ //lo sigiente sera unos Coca cola 🌮🌮🌮🌮🌮🌮🌮🌮
+float price2= 27.99;
+int quantitly2= ;
+char select2='b';
+printf("Coca cola    %f     %d        %c   \n", price2, quantitly2, select2);
+
+//Tortas chumas
+float price3
+
 
 }
+
+
+
+
