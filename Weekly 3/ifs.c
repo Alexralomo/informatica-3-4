e77efd55151183d250dc34050e90c6682674c75e
Alexralomo <manuelralomo@gmail.com>
2026-05-06 14:57:21 +0000

    ifs

---
+++ Weekly 3/ifs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void){
+   int a =67;
+   int b =67;
+   int c =10;
+
+   printf("%d == %d \n", a, b, a == b); //esto sirve para ver si es igal a los resultados enpantlla
+   printf("%d != %d is %d \n", a, b, !=c);// es cuando es no igual
+
+  int number;
+ printf(" Enter a number:");
+ Scanf("%d", &number);
+
+ if(number<0){
+
+printf("%d is a negative number.\n" ,number);
+ }else{
+    prentf("%d is a positive nomber.\n" ,number);
+    
+
+
+
+
+
+
+}
